cde1693f862aaea9f7c3986d7e4d55f5941ff288
Aiden <aidenboselaab@gmail.com>
2024-04-06 18:47:14 -0400

    Added a bit of stuff to the landing page. Will change it to look better later, just wanted to test it out.

--- hosting/src/routes/App.jsx
+++ hosting/src/routes/App.jsx
@@ -49,11 +49,18 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="d-flex p-4 flex-row hall-div">
       <img
-        src="../assets/Morehead_State_Eagles.jpg"
-        className="img-fluid"
-        alt="Dorm Room"
+        src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/andrews%20room.jpg?alt=media&token=fb6d4f80-7457-4bf8-afbf-582d128e821c"
+        className="align-self-start"
+        alt="Dorm Room" style={{height:"450px",width:"600px",borderRadius:"15px"}}
       />
+        <div className="d-flex p-4 flex-column">
+          <h2 className="align-self-start align-self-center" style={{paddingBottom:"50px"}}>Andrews Hall</h2>
+          <p className="align-self-center align-self-center">Andrews Hall offers unmatched amenities and features while offering safe, secure, and cost-effective housing for students. This four-story, air-conditioned, fully-furnished hall provides double co-joined and 4-person suites for 524 residents. With study rooms, meeting rooms, and kitchens on each floor that encourage a sense of community, residents can have many spaces to gather, study, and eat. This hall opened in 2016 as a co-ed residence hall and was named in honor of Wayne D. and Susan H. Andrews.</p>
+        </div>
+      </div>
+
     </>
   );
 }
