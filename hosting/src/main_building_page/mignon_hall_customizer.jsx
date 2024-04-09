5dd78ee86f840123367fb865800d029dc0d11e70
CaptainRainbowPizza <58518588+CaptainRainbowPizza@users.noreply.github.com>
2024-04-08 21:15:19 -0400

    Tried to do some changes to create prefab not sucessful

--- hosting/src/main_building_page/mignon_hall_customizer.jsx
+++ hosting/src/main_building_page/mignon_hall_customizer.jsx
@@ -4,29 +4,36 @@ import '../building_layouts/mignon_complex.css'; // Import CSS directly
 import InputColorPicker from '../color_picker/color_picker.jsx';
 import '../button/button.jsx';
 import ContainedButtons from '../button/button.jsx';
-import '../draggableComponents/Square.jsx'
-function MignonHallCustom() {
-    return (
-        <div className="Mignon_Complex">
-<div className="div1"> </div>
-<div className="div2"> </div>
-<div className="div3"> </div>
-<div className="div4"> </div>
-<div className="div5"> </div>
-<div className="div6"> </div>
-<div className="div7"> 
-<h1>Length</h1>
-<SliderSizes/>
-<h1>Height</h1>
-<SliderSizes/>
-<h1>Radius</h1>
-<SliderSizes/> 
-<h1>Choose Color</h1>
-<InputColorPicker/>
-<ContainedButtons/>
+import '../draggable_css/Item_One.css';
+import html2canvas from 'html2canvas'; // Import html2canvas library
 
-</div>
-</div>
+function MignonHallCustom() {
+
+    // Function to generate prefab image
+    
+
+    return (
+        <div>
+            <div className="Mignon_Complex">
+                <div className="div1"> </div>
+                <div className="div2"> </div>
+                <div className="div3"> </div>
+                <div className="div4"> </div>
+                <div className="div5"> </div>
+                <div className="div6"> </div>
+                <div className="div7"> 
+                    <h1>Length</h1>
+                    <SliderSizes/>
+                    <h1>Height</h1>
+                    <SliderSizes/>
+                    <h1>Radius</h1>
+                    <SliderSizes/> 
+                    <h1>Choose Color</h1>
+                    <InputColorPicker/>
+                    <ContainedButtons/>
+                </div>
+            </div>
+        </div>
     );
 }
 
