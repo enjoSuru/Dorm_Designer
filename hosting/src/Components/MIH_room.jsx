3e680a42a61429910d5ee47d8efd2716b3c5392c
CaptainRainbowPizza <58518588+CaptainRainbowPizza@users.noreply.github.com>
2024-04-13 14:58:15 -0400

    Added Sliders/Color Picker.

--- hosting/src/Components/MIH_room.jsx
+++ hosting/src/Components/MIH_room.jsx
@@ -2,6 +2,7 @@ import "../comp_styling/dormCSS/MIH_room.css";
 import React, { useState } from "react";
 import DraggableComponent from "../draggable/DraggableComponent";
 import Hook1 from "../draggable/hook1";
+import SliderSizes from "../sliders/sliders";
 
 export default function MIH_room() {
   return (
@@ -11,6 +12,7 @@ export default function MIH_room() {
         <div className="div1">
           <DraggableComponent />
           <Hook1 />
+          
         </div>
         <div className="div2"></div>
         <div className="div3"></div>
---
+++ hosting/src/sliders/sliders.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+export default function SliderSizes() {
+  const [defaultSliderValue, setDefaultSliderValue] = useState(50);
+
+  const handleDefaultSliderChange = (event, newValue) => {
+    setDefaultSliderValue(newValue);
+  
+   const defaultValue = defaultSliderValue;
+  };
+
+  return (
+
+    <Box sx={{ width: 300 }}>
+    
+      <Slider
+        value={defaultSliderValue}
+        onChange={handleDefaultSliderChange}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+      />
+      {/* You can access the values using smallSliderValue and defaultSliderValue */}
+      {/* For example, you can store them in variables */}
+      {/* const smallValue = smallSliderValue; */}
+      {/* const defaultValue = defaultSliderValue; */}
+    </Box>
+    
+  );
+}
