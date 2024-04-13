0a971fa4f171b9f8aa52dc6f1bffb310fe4de56c
Landon1300 <mcmaster1300@gmail.com>
2024-04-13 13:01:40 -0400

    Temp Home Done

--- hosting/src/routes/App.jsx
+++ hosting/src/routes/App.jsx
@@ -51,6 +51,35 @@ function App() {
       </div>
       <div className="d-flex p-4 flex-row hall-div">
         <img
+          src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/alumni-tower.jpg?alt=media&token=51bb778f-b4bd-454f-9e74-1e1a7a86716d"
+          className="align-self-start dorm-image"
+          alt="Dorm Room"
+        />
+        <div className="d-flex p-4 flex-column">
+          <h2
+            className="align-self-start align-self-center"
+            style={{ paddingBottom: "1px", color: "#665ecb" }}
+          >
+            Alumni Tower
+          </h2>
+          <p className="Hall-paragraph">
+            With a wrap-around patio on the main floor and community-style
+            structure, Alumni Tower is a one-of-a-kind living opportunity here
+            at MSU. This eight-story, air-conditioned, and fully-furnished hall
+            is a double occupancy facility housing 384 residents. Alumni Tower
+            serves as a break-housing opportunity for any resident who needs
+            university housing during university closed periods. Different than
+            other community-style halls, each residential floor has multiple
+            private restrooms. These lockable single-occupant restrooms have one
+            shower, one sink, and one toilet. This hall initially opened in 1967
+            as an all-male residence hall and was named in honor of Morehead
+            State University Alumni. Beginning in 2023, Alumni Tower also houses
+            the Craft Academy on several floors.
+          </p>
+        </div>
+      </div>
+      <div className="d-flex p-4 flex-row hall-div">
+        <img
           src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/andrews%20room.jpg?alt=media&token=fb6d4f80-7457-4bf8-afbf-582d128e821c"
           className="align-self-start dorm-image"
           alt="Dorm Room"
@@ -71,6 +100,32 @@ function App() {
             community, residents can have many spaces to gather, study, and eat.
             This hall opened in 2016 as a co-ed residence hall and was named in
             honor of Wayne D. and Susan H. Andrews.
+          </p>
+        </div>
+      </div>
+      <div className="d-flex p-4 flex-row hall-div">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/Cartmell.jpg?alt=media&token=fe85026f-1556-42cb-af7d-aca533e863ed"
+          className="align-self-start dorm-image"
+          alt="Dorm Room"
+        />
+        <div className="d-flex p-4 flex-column">
+          <h2
+            className="align-self-start align-self-center"
+            style={{ paddingBottom: "50px", color: "#665ecb" }}
+          >
+            Cartmell Hall
+          </h2>
+          <p className="Hall-paragraph">
+            With a wrap-around patio, a spacious first-floor lobby, and
+            community-style bathrooms, this hall encourages a sense of belonging
+            and the opportunity to create life-long friends. Beginning in Fall
+            2024, this sixteen-story, high-rise, air-conditioned hall offers
+            upperclass single-occupancy rooms. This hall opened in 1969 as an
+            all-male residence hall and was named in honor of Maysville native
+            Dr. William H. Cartmell, former Chairman of the Board of Regents.
+            During the 1970s and 1980s, many MSU fraternities had designated
+            floors in Cartmell Hall.
           </p>
         </div>
       </div>
@@ -100,6 +155,31 @@ function App() {
       </div>
       <div className="d-flex p-4 flex-row hall-div">
         <img
+          src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/fields-hall.jpg?alt=media&token=2b54863d-ec89-4c4d-9dc7-02e129fef37c"
+          className="align-self-start dorm-image"
+          alt="Dorm Room"
+        />
+        <div className="d-flex p-4 flex-column">
+          <h2
+            className="align-self-start align-self-center"
+            style={{ paddingBottom: "50px", color: "#665ecb" }}
+          >
+            Fields Hall
+          </h2>
+          <p className="Hall-paragraph">
+            With the unique cluster living design, this hall has 3 clusters of
+            rooms on each floor to support building a close-knit community. Each
+            cluster contains seven double occupancy rooms with a community
+            bathroom, laundry room, cooking facilities, and TV lounge area.
+            Fields Hall is home to the Academic Honors living-learning
+            community. This hall opened in 1928 and was named in honor of Dora
+            Fields, the first woman to serve on the Board of Regents and wife of
+            former governor William J. Fields.
+          </p>
+        </div>
+      </div>
+      <div className="d-flex p-4 flex-row hall-div">
+        <img
           src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/MaysHall.jpg?alt=media&token=a88c1079-c649-4991-9d8b-ebe7683fed5f"
           className="align-self-start dorm-image"
           alt="Dorm Room"
@@ -120,6 +200,59 @@ function App() {
             apartment features a small kitchenette with a refrigerator,
             two-burner cooktop, microwave, and individual fan controls. Laundry
             rooms are located in the central area of every floor.
+          </p>
+        </div>
+      </div>
+      <div className="d-flex p-4 flex-row hall-div">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/mignon-hall.jpg?alt=media&token=9ad14c9b-d4df-4739-8731-c4eaf7dcf21b"
+          className="align-self-start dorm-image"
+          alt="Dorm Room"
+        />
+        <div className="d-flex p-4 flex-column">
+          <h2
+            className="align-self-start align-self-center"
+            style={{ paddingBottom: "50px", color: "#665ecb" }}
+          >
+            Mignon Hall
+          </h2>
+          <p className="Hall-paragraph">
+            As part of the Mignon Complex, Mignon Hall features umbrella tables
+            in front of the building and a back patio. This six-story,
+            air-conditioned hall houses 305 students in four-person suites. Each
+            suite comes with a self-contained bathroom for four residents to
+            share. This hall opened in 1960 and was the first building of the
+            Mignon Complex to be completed. The Mignon Complex (composed of 4
+            residence halls) is named in honor of Mignon Doran, founder of MSU's
+            Personal Development Institute and wife of MSU's 7th President, Dr.
+            Adron Doran. The complex includes Mignon Hall, East Mignon Hall,
+            West Mignon Hall, and Mignon Tower.
+          </p>
+        </div>
+      </div>
+      <div className="d-flex p-4 flex-row hall-div">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/aidentestsite-af39d.appspot.com/o/mignon-tower.jpg?alt=media&token=715277e6-1590-4e21-86ca-a62c64cbc581"
+          className="align-self-start dorm-image"
+          alt="Dorm Room"
+        />
+        <div className="d-flex p-4 flex-column">
+          <h2
+            className="align-self-start align-self-center"
+            style={{ paddingBottom: "50px", color: "#665ecb" }}
+          >
+            Mignon Tower
+          </h2>
+          <p className="Hall-paragraph">
+            As part of the Mignon Complex, this fourteen-story, circular,
+            air-conditioned hall houses 296 students in four-person suites. Each
+            suite comes with a self-contained bathroom for four residents to
+            share. This hall opened in 1967 and was the final building of the
+            Mignon Complex to be completed. The Mignon Complex (composed of 4
+            residence halls) is named in honor of Mignon Doran, founder of MSU's
+            Personal Development Institute and wife of MSU's 7th President, Dr.
+            Adron Doran. While initially an all-female hall, this was the first
+            hall to be transitioned to a co-ed hall.
           </p>
         </div>
       </div>
